a5e53a1575dde473a070007313d4fa9fbc9af4b7
Ilesh Patel <ileshpatel666@gmail.com>
2025-08-16 20:24:21 +0530

    Still error in nav bar Button

--- Main.js
+++ Main.js
@@ -1,4 +1,4 @@
-const navItems = document.querySelector('.nav-item');
+const navItems = document.querySelector('.nav__items'); 
 const openNavBtn = document.querySelector('#open__nav-btn');
 const closeNavBtn = document.querySelector('#close__nav-btn');
 
